7e3adc1bc92a3504be63b7357f4ee2debf9f49d4
Vxjoe96 <josephtspringer@gmail.com>
2024-04-14 20:41:11 -0600

    Update boot.nix

--- system/hardware/boot.nix
+++ system/hardware/boot.nix
@@ -12,8 +12,12 @@
      kernelPackages = pkgs.linuxPackages_zen;
      kernelParams = [ "fastboot" "quiet" "log_level=3" "systemd.show_status=auto" "splash" "udev.log_level=3"];
      consoleLogLevel = 0;
+     plymouth.enable = true;
   };
-
+  environment.systemPackages = with pkgs; [
+     nixos-icons
+  ];
+  
   systemd.services = {
   NetworkManager-wait-online.enable = false;
   systemd-udev-settle.enable = false;
