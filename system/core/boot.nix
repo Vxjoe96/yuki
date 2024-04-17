dd185e7fedd20b8c17a732761128596e98dd238f
Vxjoe96 <josephtspringer@gmail.com>
2024-04-17 14:01:08 -0600

    Update boot.nix

--- system/core/boot.nix
+++ system/core/boot.nix
@@ -5,26 +5,39 @@
     initrd = {
       systemd.enable = true;
       supportedFilesystems = ["ntfs"];
+      verbose = false;
+      kernelModules = [ "amdgpu" ];
     };
 
     # use latest kernel
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.llinuxPackages_zen;
 
     consoleLogLevel = 3;
     kernelParams = [
       "quiet"
+      "splash"
       "systemd.show_status=auto"
       "rd.udev.log_level=3"
+      "log_level=3"
     ];
 
     loader = {
       # systemd-boot on UEFI
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
+      timeout = 0;
     };
 
     plymouth.enable = true;
+    environment.systemPackages = with pkgs; [
+     nixos-icons
+  ];
 
     tmp.cleanOnBoot = true;
+    systemd.services = {
+      NetworkManager-wait-online.enable = false;
+      systemd-udev-settle.enable = false;
+      };
   };
+
 }
